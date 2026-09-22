6f2830742ec059142f4236be739df3a9056b7c30
Yves Pares <yves.pares@aletiq.com>
2026-09-22 15:11:04 +0200

    Fix client threads generation in main

--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,15 @@ use marble_test::mpmc;
 
 fn main() {
     let q: mpmc::Queue<u32> = mpmc::Queue::default();
-    for client_id in 0..7 {
+    for client_id in 0..=7 {
         let q_client = q.clone();
         thread::spawn(move || {
-            // Each client sleeps a different amount of time
-            thread::sleep(Duration::from_secs(1 + (client_id as u64)));
-            let msg = q_client.read(client_id);
-            println!("Client {} read: {:?}", client_id, msg);
+            loop {
+                // Each client sleeps a different amount of time
+                thread::sleep(Duration::from_secs(1 + (client_id as u64)));
+                let msg = q_client.read(client_id);
+                println!("Client {} read: {:?}", client_id, msg);
+            }
         });
     }
     for msg in 0..100000 {
@@ -23,4 +25,3 @@ fn main() {
         thread::sleep(Duration::from_secs(2));
     }
 }
-
